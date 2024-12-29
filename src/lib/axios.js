d0526a3a43ce4f10271d098c2050f0dca52f6612
ns0851 <ns0851@srmist.edu.in>
2024-12-29 15:44:07 +0530

    Fix vercel.json for deployment

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/",
+  baseURL: "https://chat-app-backend-correct.vercel.app/",
   withCredentials: true,
 });
